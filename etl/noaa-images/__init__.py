b785b33d3c96e7ce5a1adc5d36476da17da55938
Tevin Brown <tevinbrown@wta-mbp579.attlocal.net>
2021-02-25 09:15:45 -0500

    removing flask, next step is using Django

--- etl/noaa-images/__init__.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup, find_packages
-setup(
-    name='noaa-images',
-    packages=find_packages(),
-)
-from util import assets
-from .views import app
--- etl/start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Script to download run the backend
-source virtual/bin/activate
-which python
-export FLASK_APP=noaa-images
-flask run
