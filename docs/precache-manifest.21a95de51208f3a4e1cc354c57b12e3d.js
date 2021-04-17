e1557d863bd59c70bae7adb4b165872ca967c1db
Tevin Brown <tevinbrown@wta-mbp579.attlocal.net>
2021-04-17 15:17:44 -0400

    attempting to deploy

---
+++ docs/precache-manifest.21a95de51208f3a4e1cc354c57b12e3d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c278112268848b8ccdb71a581f24b487",
+    "url": "/cocoa-clouds/index.html"
+  },
+  {
+    "revision": "92b94f787bcbce877966",
+    "url": "/cocoa-clouds/static/css/main.66fb87b7.chunk.css"
+  },
+  {
+    "revision": "949935089399ce2a534e",
+    "url": "/cocoa-clouds/static/js/2.365a44fc.chunk.js"
+  },
+  {
+    "revision": "eadc8ac8b6080df510389a008a76ce0a",
+    "url": "/cocoa-clouds/static/js/2.365a44fc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "92b94f787bcbce877966",
+    "url": "/cocoa-clouds/static/js/main.7fe4053e.chunk.js"
+  },
+  {
+    "revision": "b5c9b910f991bda49b36",
+    "url": "/cocoa-clouds/static/js/runtime-main.a9826f6b.js"
+  },
+  {
+    "revision": "714253c8ffef92fbb17891f1ba531831",
+    "url": "/cocoa-clouds/static/media/Logo.714253c8.png"
+  }
+]);
